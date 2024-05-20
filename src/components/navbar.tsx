cac946d810e3125971eeeff9bf83d813c68bc623
theblvckdev <nathancodes05@gmail.com>
2024-05-20 14:29:32 +0100

    sidebar and navbar code update [used context to toggle sidebar]

---
+++ src/components/navbar.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import Image from "next/image";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { IoCloseOutline, IoMenuOutline } from "react-icons/io5";
+import Logo from "./logo";
+import avatar from "@/assets/images/avatar.jpg";
+import React, { useState } from "react";
+import useAppContext from "@/hooks/use_app_context";
+
+function classNames(
+  ...classes: (string | boolean | null | undefined)[]
+): string {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Navbar: React.FC = () => {
+  const { state, toggleIsOpen } = useAppContext();
+
+  return (
+    <Disclosure as="nav" className="bg-transparent">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button
+                  onClick={toggleIsOpen}
+                  className={`relative inline-flex items-center justify-center rounded-md p-2 ${
+                    state.isOpen ? "text-white" : "text-black"
+                  } outline-none`}
+                >
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <IoCloseOutline
+                      className="block h-7 w-7"
+                      aria-hidden="true"
+                    />
+                  ) : (
+                    <IoMenuOutline
+                      className="block h-7 w-7"
+                      aria-hidden="true"
+                    />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center lg:items-stretch lg:justify-start">
+                <Logo />
+              </div>
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                {/* Profile dropdown */}
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="relative flex rounded-full text-sm outline-none">
+                      <span className="absolute -inset-1.5" />
+                      <span className="sr-only">Open user menu</span>
+                      <Image
+                        className="h-8 w-8 rounded-full object-cover"
+                        src={avatar}
+                        alt=""
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as="div"
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Settings
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block px-4 py-2 text-sm text-gray-700"
+                            )}
+                          >
+                            Sign out
+                          </a>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </Disclosure>
+  );
+};
+
+export default Navbar;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import useAppContext from "@/hooks/use_app_context";
+import React from "react";
+
+const Sidebar = () => {
+  const { state } = useAppContext();
+
+  return (
+    <aside
+      className={`lg:w-full lg:relative fixed h-screen md:w-[50%] w-[70%] lg:left-0 ${
+        state.isOpen ? "left-0" : "-left-[100%]"
+      } bg-black duration-300 ease-in`}
+    ></aside>
+  );
+};
+
+export default Sidebar;
